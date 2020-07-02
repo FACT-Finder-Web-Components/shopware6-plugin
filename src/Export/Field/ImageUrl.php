632278aa3ac0061eb6290c5f68f875d133909448
Francesco Marangi <2734054+fmarangi@users.noreply.github.com>
2020-07-02 16:45:12 +0200

    Export relative URLs

--- src/Export/Field/ImageUrl.php
+++ src/Export/Field/ImageUrl.php
@@ -4,19 +4,10 @@ declare(strict_types=1);
 
 namespace Omikron\FactFinder\Shopware6\Export\Field;
 
-use Omikron\FactFinder\Shopware6\Export\DomainService;
 use Shopware\Core\Content\Product\SalesChannel\SalesChannelProductEntity as Product;
 
 class ImageUrl implements FieldInterface
 {
-    /** @var DomainService */
-    private $domainService;
-
-    public function __construct(DomainService $domainService)
-    {
-        $this->domainService = $domainService;
-    }
-
     public function getName(): string
     {
         return 'ImageUrl';
