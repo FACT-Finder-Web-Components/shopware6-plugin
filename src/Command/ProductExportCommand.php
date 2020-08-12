4432fe577bf57d6d6bcf04f5193f8a608e241519
Aleksander Laurowski <ext-a.laurowski@omikron.net>
2020-08-12 06:47:48 +0200

    FFWEB-1744 merge console commands

--- src/Command/ProductExportCommand.php
+++ src/Command/ProductExportCommand.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Omikron\FactFinder\Shopware6\Command;
 
+use Omikron\FactFinder\Shopware6\Communication\PushImportService;
 use Omikron\FactFinder\Shopware6\Export\FeedFactory;
 use Omikron\FactFinder\Shopware6\Export\SalesChannelService;
 use Omikron\FactFinder\Shopware6\Export\Stream\ConsoleOutput;
+use Omikron\FactFinder\Shopware6\Export\Stream\CsvFile;
+use Omikron\FactFinder\Shopware6\Upload\UploadService;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -17,24 +20,57 @@ class ProductExportCommand extends Command implements ContainerAwareInterface
 {
     use ContainerAwareTrait;
 
+    private const UPLOAD_FEED_OPTION = 'upload';
+    private const PUSH_IMPORT_OPTION = 'import';
+
     /** @var SalesChannelService */
     private $channelService;
 
     /** @var FeedFactory */
     private $feedFactory;
 
+    /** @var UploadService */
+    private $uploadService;
+
+    /** @var PushImportService */
+    private $pushImportService;
+
     public function __construct(
         SalesChannelService $channelService,
-        FeedFactory $feedFactory
+        FeedFactory $feedFactory,
+        PushImportService $pushImportService,
+        UploadService $uploadService
     ) {
         parent::__construct('factfinder:export:products');
-        $this->channelService = $channelService;
-        $this->feedFactory    = $feedFactory;
+        $this->channelService    = $channelService;
+        $this->feedFactory       = $feedFactory;
+        $this->pushImportService = $pushImportService;
+        $this->uploadService     = $uploadService;
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('Export articles feed.');
+        $this->addOption(self::UPLOAD_FEED_OPTION, 'u', InputOption::VALUE_NONE, 'Should upload after exporting');
+        $this->addOption(self::PUSH_IMPORT_OPTION, 'i', InputOption::VALUE_NONE, 'Should import after uploading');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $feed = $this->feedFactory->create($this->channelService->getSalesChannelContext());
-        $feed->generate(new ConsoleOutput($output), $this->container->getParameter('factfinder.export.columns'));
+        $feedService = $this->feedFactory->create($this->channelService->getSalesChannelContext());
+
+        if ($input->getOption(self::UPLOAD_FEED_OPTION)) {
+            $fileHandle = tmpfile();
+            $feedService->generate(new CsvFile($fileHandle), $this->container->getParameter('factfinder.export.columns'));
+            $this->uploadService->upload($fileHandle);
+            $output->writeln('Feed has been succesfully uploaded');
+
+            if ($input->getOption(self::PUSH_IMPORT_OPTION)) {
+                $this->pushImportService->execute();
+                $output->writeln('FACT-Finder import has been start');
+            }
+        } else {
+            $feedService->generate(new ConsoleOutput($output), $this->container->getParameter('factfinder.export.columns'));
+        }
     }
 }
