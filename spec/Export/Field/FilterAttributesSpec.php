c0c1be7d9abe6b040adc3ff67623bc0644d00c5a
Aleksander Laurowski <ext-a.laurowski@omikron.net>
2020-09-21 07:06:41 +0200

    FFWEB-1804 add more unit tests

---
+++ spec/Export/Field/FilterAttributesSpec.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace spec\Omikron\FactFinder\Shopware6\Export\Field;
+
+use Omikron\FactFinder\Shopware6\Export\PropertyFormatter;
+use PhpSpec\ObjectBehavior;
+use Shopware\Core\Content\Product\ProductCollection;
+use Shopware\Core\Content\Product\SalesChannel\SalesChannelProductEntity as Product;
+use Shopware\Core\Content\Property\Aggregate\PropertyGroupOption\PropertyGroupOptionCollection;
+
+class FilterAttributesSpec extends ObjectBehavior
+{
+    function let(PropertyFormatter $propertyFormatter)
+    {
+        $this->beConstructedWith($propertyFormatter);
+    }
+
+    function it_does_not_fail_if_product_have_no_properties(
+        Product $product,
+        ProductCollection $productCollection,
+        PropertyGroupOptionCollection $emptyProperties
+    ) {
+        $product->getChildren()->willReturn($productCollection);
+        $emptyProperties->getElements()->willReturn([]);
+        $product->getProperties()->willReturn($emptyProperties);
+        $this->shouldNotThrow()->during('getValue', [$product]);
+    }
+}
