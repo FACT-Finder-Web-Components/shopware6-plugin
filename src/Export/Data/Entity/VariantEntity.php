f08314d46227fceddfd3673f962de099a073ebba
a.laurowski <ext-a.laurowski@omikron.net>
2021-04-22 09:25:38 +0200

    FFWEB-2012 export custom fields from variants

--- src/Export/Data/Entity/VariantEntity.php
+++ src/Export/Data/Entity/VariantEntity.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Omikron\FactFinder\Shopware6\Export\Data\Entity;
 
 use Omikron\FactFinder\Shopware6\Export\Data\ExportEntityInterface;
+use Omikron\FactFinder\Shopware6\Export\Field\FieldInterface;
 use Omikron\FactFinder\Shopware6\Export\PropertyFormatter;
 use Shopware\Core\Content\Product\SalesChannel\SalesChannelProductEntity as Product;
 use function array_map as map;
@@ -20,11 +21,19 @@ class VariantEntity implements ExportEntityInterface
     /** @var PropertyFormatter */
     private $propertyFormatter;
 
-    public function __construct(Product $product, array $parentData, PropertyFormatter $propertyFormatter)
-    {
+    /** @var FieldInterface[] */
+    private $variantFields;
+
+    public function __construct(
+        Product $product,
+        array $parentData,
+        PropertyFormatter $propertyFormatter,
+        iterable $variantFields
+    ) {
         $this->product           = $product;
         $this->parentData        = $parentData;
         $this->propertyFormatter = $propertyFormatter;
+        $this->variantFields     = $variantFields;
     }
 
     public function getId(): string
@@ -35,6 +44,8 @@ class VariantEntity implements ExportEntityInterface
     public function toArray(): array
     {
         $opts = '|' . implode('|', map($this->propertyFormatter, $this->product->getOptions()->getElements())) . '|';
-        return ['ProductNumber' => $this->product->getProductNumber(), 'FilterAttributes' => $opts] + $this->parentData;
+        return array_reduce($this->variantFields, function (array $fields, FieldInterface $field) {
+            return [$field->getName() => $field->getValue($this->product)] + $fields;
+        }, ['ProductNumber' => $this->product->getProductNumber(), 'FilterAttributes' => $opts] + $this->parentData);
     }
 }
