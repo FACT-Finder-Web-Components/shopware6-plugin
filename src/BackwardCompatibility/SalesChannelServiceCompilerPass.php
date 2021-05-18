4a48e2e51e47509b04866a67fba10771d8abd925
Pavel Novikov <paulfcdd@gmail.com>
2021-05-18 12:11:23 +0200

    FFWEB-2041 cs-fix update config

--- src/BackwardCompatibility/SalesChannelServiceCompilerPass.php
+++ src/BackwardCompatibility/SalesChannelServiceCompilerPass.php
@@ -15,8 +15,7 @@ class SalesChannelServiceCompilerPass implements CompilerPassInterface
     public function process(ContainerBuilder $container)
     {
         if ($container->has('Shopware\Core\System\SalesChannel\Context\CachedSalesChannelContextFactory')) {
-            $salesChannelService  = new Definition(
-                'Omikron\FactFinder\Shopware6\BackwardCompatibility\Extension\CachedSalesChannelContextFactoryExtension', [
+            $salesChannelService  = new Definition('Omikron\FactFinder\Shopware6\BackwardCompatibility\Extension\CachedSalesChannelContextFactoryExtension', [
                 new Reference('Shopware\Core\System\SalesChannel\Context\CachedSalesChannelContextFactory'),
                 new Reference('tag_aware_cache'),
                 new Reference('Shopware\Core\Framework\Adapter\Cache\CacheTracer'),
