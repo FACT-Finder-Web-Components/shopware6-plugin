4bda110491803f3a36fba3873d0ff1524f60c25d
Aleksander Laurowski <ext-a.laurowski@omikron.net>
2021-05-20 16:06:46 +0200

    added 6.4 support
    fixup! added 6.4 support

--- src/BackwardCompatibility/SalesChannelServiceCompilerPass.php
+++ src/BackwardCompatibility/SalesChannelServiceCompilerPass.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Omikron\FactFinder\Shopware6\BackwardCompatibility;
 
 use Omikron\FactFinder\Shopware6\Export\SalesChannelService;
+use Omikron\FactFinder\Shopware6\Subscriber\CategoryView;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
@@ -13,13 +14,20 @@ class SalesChannelServiceCompilerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
-        $salesChannelService   = $container->getDefinition(SalesChannelService::class);
-        $channelContextFactory = new Definition('Omikron\FactFinder\Shopware6\BackwardCompatibility\Extension\SalesChannelContextFactory', $container->getDefinition('Shopware\Core\System\SalesChannel\Context\SalesChannelContextFactory')->getArguments());
+        $salesChannelService    = $container->getDefinition(SalesChannelService::class);
+        $categoryViewSubscriber = $container->getDefinition(CategoryView::class);
+        $channelContextFactory  = new Definition('Omikron\FactFinder\Shopware6\BackwardCompatibility\Extension\SalesChannelContextFactory', $container->getDefinition('Shopware\Core\System\SalesChannel\Context\SalesChannelContextFactory')->getArguments());
 
         if ($container->has('Shopware\Core\System\SalesChannel\Context\CachedSalesChannelContextFactory')) {
             $channelContextFactory = new Definition('Omikron\FactFinder\Shopware6\BackwardCompatibility\Extension\CachedSalesChannelContextFactory', $container->getDefinition('Shopware\Core\System\SalesChannel\Context\CachedSalesChannelContextFactory')->getArguments());
         }
 
         $salesChannelService->setArgument('$channelContextFactory', $channelContextFactory);
+        $categoryViewSubscriber->setArgument(
+            '$cmsPageRoute',
+            $container->has('Shopware\Core\Content\Category\SalesChannel\CachedCategoryRoute')
+                ? $container->getDefinition('Shopware\Core\Content\Category\SalesChannel\CachedCategoryRoute')
+                : $container->getDefinition('Shopware\Core\Content\Category\SalesChannel\CategoryRoute')
+        );
     }
 }
