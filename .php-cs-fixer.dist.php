e6356d82617cbb6235aa350cfb6797b1ed0837fa
paulfcdd <Ctrnjh56ufpf!>
2021-05-27 14:01:47 +0200

    FFWEB-2020: cs-fixer config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -15,22 +15,25 @@ return (new Config())
     ->setHideProgress(true)
     ->setUsingCache(false)
     ->setRules([
-        '@PSR2' => true,
-        '@Symfony' => true,
-        'array_syntax' => ['syntax' => 'short'],
+        '@PSR2'                  => true,
+        '@Symfony'               => true,
+        'array_syntax'           => ['syntax' => 'short'],
+        'binary_operator_spaces' => [
+            'operators' => ['=>' => 'align', '==' => 'align'],
+        ],
 //        'binary_operator_spaces' => ['align_double_arrow' => true, 'align_equals' => true],
         'blank_line_after_opening_tag' => true,
-        'blank_line_before_statement' => ['statements' => ['break', 'continue', 'declare', 'throw', 'try']],
-        'concat_space' => ['spacing' => 'one'],
-        'declare_strict_types' => true,
-        'increment_style' => ['style' => 'post'],
-        'no_superfluous_phpdoc_tags' => false,
-        'no_useless_else' => true,
-        'no_useless_return' => true,
-        'ordered_class_elements' => true,
-        'ordered_imports' => ['imports_order' => ['class', 'function', 'const']],
-        'strict_comparison' => true,
-        'yoda_style' => ['equal' => false, 'identical' => false, 'less_and_greater' => false],
+        'blank_line_before_statement'  => ['statements' => ['break', 'continue', 'declare', 'throw', 'try']],
+        'concat_space'                 => ['spacing' => 'one'],
+        'declare_strict_types'         => true,
+        'increment_style'              => ['style' => 'post'],
+        'no_superfluous_phpdoc_tags'   => false,
+        'no_useless_else'              => true,
+        'no_useless_return'            => true,
+        'ordered_class_elements'       => true,
+        'ordered_imports'              => ['imports_order' => ['class', 'function', 'const']],
+        'strict_comparison'            => true,
+        'yoda_style'                   => ['equal' => false, 'identical' => false, 'less_and_greater' => false],
     ])
     ->setFinder($finder)
     ;
