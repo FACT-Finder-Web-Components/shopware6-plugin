18d7983ec2421efb7eee2ba26452ba034be03f85
Aleksander Laurowski <ext-a.laurowski@omikron.net>
2021-05-27 07:21:25 +0200

    fixup! FFWEB-2049 Price export based on cart rules

--- spec/Export/Field/PriceSpec.php
+++ spec/Export/Field/PriceSpec.php
@@ -8,6 +8,7 @@ use Omikron\FactFinder\Shopware6\Export\Field\FieldInterface;
 use Omikron\FactFinder\Shopware6\Export\Formatter\NumberFormatter;
 use PhpSpec\ObjectBehavior;
 use Shopware\Core\Checkout\Cart\Price\Struct\CalculatedPrice;
+use Shopware\Core\Checkout\Cart\Price\Struct\PriceCollection;
 use Shopware\Core\Content\Product\SalesChannel\SalesChannelProductEntity as Product;
 
 class PriceSpec extends ObjectBehavior
@@ -27,10 +28,30 @@ class PriceSpec extends ObjectBehavior
         $this->getName()->shouldReturn('Price');
     }
 
-    function it_gets_the_product_price(Product $product, CalculatedPrice $price, NumberFormatter $numberFormatter)
-    {
+    function it_gets_the_calculated_product_price_if_no_calculated_prices(
+        Product $product,
+        CalculatedPrice $price,
+        PriceCollection $priceCollection,
+        NumberFormatter $numberFormatter
+    ) {
+        $product->getCalculatedPrices()->willReturn($priceCollection);
+        $priceCollection->count()->willReturn(0);
         $product->getCalculatedPrice()->willReturn($price);
         $price->getTotalPrice()->willReturn(pi());
+        $numberFormatter->format(pi())->willReturn('3.14');
+        $this->getValue($product)->shouldReturn('3.14');
+    }
+
+    function it_gets_the_first_calculated_price_if_exist(
+        Product $product,
+        PriceCollection $priceCollection,
+        CalculatedPrice $price,
+        NumberFormatter $numberFormatter
+    ) {
+        $priceCollection->count()->willReturn(1);
+        $product->getCalculatedPrices()->willReturn($priceCollection);
+        $priceCollection->first()->willReturn($price);
+        $price->getUnitPrice()->willReturn(pi());
         $numberFormatter->format(pi())->willReturn('3.14');
         $this->getValue($product)->shouldReturn('3.14');
     }
--- src/Export/Field/Price.php
+++ src/Export/Field/Price.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Omikron\FactFinder\Shopware6\Export\Field;
 
 use Omikron\FactFinder\Shopware6\Export\Formatter\NumberFormatter;
-use Omikron\FactFinder\Shopware6\Export\SalesChannelService;
-use Shopware\Core\Content\Product\SalesChannel\Price\ProductPriceCalculator;
 use Shopware\Core\Content\Product\SalesChannel\SalesChannelProductEntity as Product;
 
 class Price implements FieldInterface
@@ -14,20 +12,8 @@ class Price implements FieldInterface
     /** @var NumberFormatter */
     private $numberFormatter;
 
-    /** @var ProductPriceCalculator */
-    private $priceCalculator;
-
-    /** @var SalesChannelService */
-    private $channelService;
-
-    public function __construct(
-        NumberFormatter $numberFormatter,
-        ProductPriceCalculator $priceCalculator,
-        SalesChannelService $channelService
-    ) {
+    public function __construct(NumberFormatter $numberFormatter) {
         $this->numberFormatter = $numberFormatter;
-        $this->priceCalculator = $priceCalculator;
-        $this->channelService  = $channelService;
     }
 
     public function getName(): string
@@ -37,7 +23,6 @@ class Price implements FieldInterface
 
     public function getValue(Product $product): string
     {
-        $this->priceCalculator->calculate([$product], $this->channelService->getSalesChannelContext());
         if ($product->getCalculatedPrices()->count() === 0) {
             return $this->numberFormatter->format((float)$product->getCalculatedPrice()->getTotalPrice());
         }
