59f1b033b315e954d85a9883b46d42436526a9a6
paulfcdd <Ctrnjh56ufpf!>
2021-08-11 13:56:40 +0200

    FFWEB-2134: Add  test

--- spec/Export/Field/Brand/BrandLogoSpec.php
+++ spec/Export/Field/Brand/BrandLogoSpec.php
@@ -4,6 +4,8 @@ namespace spec\Omikron\FactFinder\Shopware6\Export\Field\Brand;
 
 use PhpSpec\ObjectBehavior;
 use Omikron\FactFinder\Shopware6\Export\Field\Brand\FieldInterface;
+use Shopware\Core\Content\Media\MediaEntity as Media;
+use Shopware\Core\Content\Product\Aggregate\ProductManufacturer\ProductManufacturerEntity as Brand;
 
 class BrandLogoSpec extends ObjectBehavior
 {
@@ -15,5 +17,14 @@ class BrandLogoSpec extends ObjectBehavior
     function it_has_a_name()
     {
         $this->getName()->shouldReturn('BrandLogo');
+    }
+    function it_does_not_fail_if_the_image_is_not_present(Brand $brand, Media $media)
+    {
+        $this->shouldNotThrow()->during('getValue', [$brand]);
+        $this->getValue($brand)->shouldReturn('');
+
+        $brand->getMedia()->willReturn($media);
+        $media->getUrl()->willReturn('/brand_image.jpg');
+        $this->getValue($brand)->shouldReturn('/brand_image.jpg');
     }
 }
