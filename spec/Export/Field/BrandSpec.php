e1844a8ac141a6c4934492bc9cda99d8562cbb14
Aleksander Laurowski <ext-a.laurowski@omikron.net>
2021-08-18 14:36:45 +0200

    fixup! FFWEB-2134 rework creating feeds and export entities

--- spec/Export/Field/BrandSpec.php
+++ spec/Export/Field/BrandSpec.php
@@ -6,7 +6,6 @@ use Omikron\FactFinder\Shopware6\Export\Field\FieldInterface;
 use PhpSpec\ObjectBehavior;
 use Shopware\Core\Content\Product\Aggregate\ProductManufacturer\ProductManufacturerEntity as Manufacturer;
 use Shopware\Core\Content\Product\SalesChannel\SalesChannelProductEntity as Product;
-use Shopware\Core\Framework\DataAbstractionLayer\Entity;
 
 class BrandSpec extends ObjectBehavior
 {
@@ -20,24 +19,24 @@ class BrandSpec extends ObjectBehavior
         $this->getName()->shouldReturn('Brand');
     }
 
-    function it_does_not_fail_if_the_brand_is_not_present(Entity $entity, Manufacturer $manufacturer)
+    function it_does_not_fail_if_the_brand_is_not_present(Product $product, Manufacturer $manufacturer)
     {
-        $this->shouldNotThrow()->during('getValue', [$entity]);
-        $this->getValue($entity)->shouldReturn('');
+        $this->shouldNotThrow()->during('getValue', [$product]);
+        $this->getValue($product)->shouldReturn('');
 
-        $entity->getManufacturer()->willReturn($manufacturer);
-        $this->shouldNotThrow()->during('getValue', [$entity]);
-        $this->getValue($entity)->shouldReturn('');
+        $product->getManufacturer()->willReturn($manufacturer);
+        $this->shouldNotThrow()->during('getValue', [$product]);
+        $this->getValue($product)->shouldReturn('');
 
         $manufacturer->getTranslation('name')->willReturn('ACME Inc.');
-        $this->shouldNotThrow()->during('getValue', [$entity]);
-        $this->getValue($entity)->shouldReturn('ACME Inc.');
+        $this->shouldNotThrow()->during('getValue', [$product]);
+        $this->getValue($product)->shouldReturn('ACME Inc.');
     }
 
-    function it_gets_the_value_from_the_manufacturer(Entity $entity, Manufacturer $manufacturer)
+    function it_gets_the_value_from_the_manufacturer(Product $product, Manufacturer $manufacturer)
     {
-        $entity->getManufacturer()->willReturn($manufacturer);
+        $product->getManufacturer()->willReturn($manufacturer);
         $manufacturer->getTranslation('name')->willReturn('FACT-Finder');
-        $this->getValue($entity)->shouldReturn('FACT-Finder');
+        $this->getValue($product)->shouldReturn('FACT-Finder');
     }
 }
