ffab944b8cf5acbae14459b98f60f87b035df235
a.laurowski <ext-a.laurowski@omikron.net>
2021-10-15 17:32:22 +0200

    FFWEB-2249 fix configuration subscriber does not get channel with sales channel id argument

---
+++ spec/Subscriber/ConfigurationSubscriberSpec.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Omikron\FactFinder\Shopware6\Subscriber;
+
+use Omikron\FactFinder\Shopware6\Config\Communication;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Prophecy\Argument\Token\LogicalAndToken;
+use Prophecy\Argument\Token\TypeToken;
+use Shopware\Core\Checkout\Customer\CustomerEntity;
+use Shopware\Core\Framework\Struct\ArrayEntity;
+use Shopware\Core\Framework\Struct\Struct;
+use Shopware\Core\System\Currency\CurrencyEntity;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Shopware\Core\System\SalesChannel\SalesChannelEntity;
+use Shopware\Storefront\Page\GenericPageLoadedEvent;
+use Shopware\Storefront\Page\Page;
+use Symfony\Component\HttpFoundation\Request;
+
+class ConfigurationSubscriberSpec extends ObjectBehavior
+{
+    function let(Communication $communication)
+    {
+        $fieldRoles              = [];
+        $communicationParameters = [];
+        $communication->getServerUrl()->willReturn('https://factfinder.server.com');
+        $this->beConstructedWith($communication, $fieldRoles, $communicationParameters);
+    }
+
+    function it_will_return_factfinderchannel_for_specific_sales_channel_id(
+        Communication          $communication,
+        GenericPageLoadedEvent $event,
+        SalesChannelContext    $salesChannelContext,
+        SalesChannelEntity     $salesChannel,
+        CustomerEntity         $customer,
+        CurrencyEntity         $currency,
+        Request                $request,
+        Struct                 $extension,
+        Page                   $page
+    ) {
+        $event->getSalesChannelContext()->willReturn($salesChannelContext);
+        $salesChannelContext->getCustomer()->willReturn($customer);
+        $customer->getId()->willReturn(1);
+        $salesChannelContext->getCurrency()->willReturn($currency);
+        $currency->getIsoCode()->willReturn('EUR');
+        $salesChannelContext->getSalesChannel()->willReturn($salesChannel);
+        $salesChannel->getId()->willReturn('main_sales_channel');
+        $communication->getChannel('main_sales_channel')->willReturn('some_ff_channel');
+        $event->getRequest()->willReturn($request);
+        $request->get('_route')->willReturn('factfinder');
+        $request->getLocale()->willReturn('en');
+        $event->getPage()->willReturn($page);
+
+        $page->addExtension(
+            'factfinder',
+            new LogicalAndToken(
+                [
+                    new TypeToken(ArrayEntity::class),
+                    Argument::withEntry('communication', Argument::withEntry('channel', 'some_ff_channel'))
+                ]
+            ))->shouldBeCalled();
+
+        $this->onPageLoaded($event);
+    }
+}
