1945d4192f94719514f12794a17126d6356a01e6
Pavel Novikov <paulfcdd@gmail.com>
2021-11-18 16:07:36 +0100

    FFWEB-2224: cr fix

--- src/Subscriber/CategoryView.php
+++ src/Subscriber/CategoryView.php
@@ -46,21 +46,40 @@ class CategoryView implements EventSubscriberInterface
         $path             = $this->getPath($category);
         $disableImmediate = safeGetByName($category->getCustomFields())(OmikronFactFinder::DISABLE_SEARCH_IMMEDIATE_CUSTOM_FIELD_NAME);
         $isHome           = $event->getRequest()->get('_route') === 'frontend.home.page';
+        $addParams = $this->prepareAddParams();
+        $communication = [
+            'search-immediate' => !$isHome && !$disableImmediate ? 'true' : 'false',
+            'category-page'    => $this->prepareCategoryPath($path),
+        ];
+
+        if ($addParams) {
+            $communication['add-params'] = $addParams;
+        }
 
         $event->getPage()->getExtension('factfinder')->assign(
             [
-                'communication' => [
-                    'search-immediate' => !$isHome && !$disableImmediate ? 'true' : 'false',
-                    'add-params'       => $this->prepareAddParams($path),
-                    'category-page'    => $this->prepareCategoryPath($path),
-                ],
+                'communication' => $communication,
             ]);
     }
 
-    private function prepareAddParams($path): string
+    private function prepareAddParams(): ?string
     {
-        $filterValue = is_int(strpos($this->fieldName, 'CategoryPath')) ? '\'\'' : urlencode($this->fieldName . ':' . $path);
-        return $path ? implode(',', $this->initial + [sprintf('filter=%s', $filterValue)]) : '';
+        if (empty($this->initial)) {
+            return null;
+        }
+        $paramsString = '';
+        $i=0;
+        $initialCount = count($this->initial);
+
+        foreach ($this->initial as $key=>$value) {
+            $i++;
+            $paramsString .= $key . '=' .$value;
+            if ($i<$initialCount) {
+                $paramsString .= ',';
+            }
+        }
+        return $paramsString;
+
     }
 
     private function prepareCategoryPath($path): string
