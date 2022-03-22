455eabbdd891d5f1bf11268baee3df774ed2c0e1
Aleksander Laurowski <ext-a.laurowski@fact-finder.com>
2022-03-22 11:57:26 +0100

    FFWEB-2443 add layout column to cms export

---
+++ src/Export/Field/Layout.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Omikron\FactFinder\Shopware6\Export\Field;
+
+use Omikron\FactFinder\Shopware6\Export\Filter\TextFilter;
+use Shopware\Core\Content\Category\CategoryEntity;
+use Shopware\Core\Content\Cms\Aggregate\CmsBlock\CmsBlockEntity;
+use Shopware\Core\Content\Cms\Aggregate\CmsSection\CmsSectionEntity;
+use Shopware\Core\Content\Cms\Aggregate\CmsSlot\CmsSlotEntity;
+use Shopware\Core\Framework\DataAbstractionLayer\Entity;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityCollection;
+use function Omikron\FactFinder\Shopware6\Internal\Utils\flatMap;
+use function Omikron\FactFinder\Shopware6\Internal\Utils\safeGetByName;
+
+class Layout implements FieldInterface
+{
+    private TextFilter $textFilter;
+
+    public function __construct(TextFilter $textFilter)
+    {
+        $this->textFilter = $textFilter;
+    }
+
+    public function getName(): string
+    {
+        return 'Layout';
+    }
+
+    /**
+     * @param CategoryEntity $entity
+     *
+     * @return string
+     */
+    public function getValue(Entity $entity): string
+    {
+        $layout = array_map(
+            fn (CmsSlotEntity $slot) => safeGetByName(safeGetByName($slot->getConfig(), 'content'), 'value'),
+            flatMap(
+                fn (CmsBlockEntity $block): array => $this->toValues($block->getSlots()),
+                flatMap(
+                    fn (CmsSectionEntity $section): array => $this->toValues($section->getBlocks()),
+                    $this->toValues($entity->getCmsPage()->getSections())
+                )
+            )
+        );
+
+        return $this->textFilter->filterValue(implode(' ', array_filter($layout)));
+    }
+
+    public function getCompatibleEntityTypes(): array
+    {
+        return [CategoryEntity::class];
+    }
+
+    private function toValues(EntityCollection $collection): array
+    {
+        /**
+         * @param CmsBlockEntity|CmsSectionEntity $current
+         * @param CmsBlockEntity|CmsSectionEntity $next
+         *
+         * @return bool
+         */
+        $sortByPosition = fn ($current, $next): bool => !method_exists($current, 'getPosition') || $current->getPosition() > $next->getPosition();
+        $collection->sort($sortByPosition);
+
+        return array_values($collection->getElements());
+    }
+}
