c006f9f3d6a089ba8ee5137a66559dfc0d6f88dc
gjamroz <gjamroz@objectivity.co.uk>
2023-01-20 08:03:38 +0100

    FFWEB-2647: set RequestStack as optional to avid fails for CLI-Commands

--- src/Subscriber/CustomerLoginEventSubscriber.php
+++ src/Subscriber/CustomerLoginEventSubscriber.php
@@ -10,9 +10,9 @@ use Symfony\Component\HttpFoundation\RequestStack;
 
 class CustomerLoginEventSubscriber implements EventSubscriberInterface
 {
-    private RequestStack $requestStack;
+    private ?RequestStack $requestStack;
 
-    public function __construct(RequestStack $requestStack)
+    public function __construct(?RequestStack $requestStack = null)
     {
         $this->requestStack = $requestStack;
     }
@@ -26,7 +26,10 @@ class CustomerLoginEventSubscriber implements EventSubscriberInterface
 
     public function hasJustLoggedIn(): void
     {
-        if ($this->requestStack->getMainRequest() === null) {
+        if (
+            !isset($this->requestStack)
+            || $this->requestStack->getMainRequest() === null
+        ) {
             return;
         }
 
