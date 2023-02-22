fd443eb7114bd8dbf61e4ded5dae32386e016329
gjamroz <gjamroz@objectivity.co.uk>
2023-02-22 15:07:44 +0100

    FFWEB-2669: Redirect mapping for selected queries

--- src/Config/ExtensionConfig.php
+++ src/Config/ExtensionConfig.php
@@ -5,9 +5,20 @@ declare(strict_types=1);
 namespace Omikron\FactFinder\Shopware6\Config;
 
 use Omikron\FactFinder\Shopware6\Domain\RedirectMapping;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class ExtensionConfig extends BaseConfig
 {
+    private Request $request;
+
+    public function __construct(SystemConfigService $systemConfig, ?RequestStack $requestStack = null)
+    {
+        $this->request = $requestStack ? $requestStack->getCurrentRequest() : new Request();
+        parent::__construct($systemConfig);
+    }
+
     public function getTrackingSettings(): array
     {
         return [
@@ -19,6 +30,11 @@ class ExtensionConfig extends BaseConfig
 
     public function getRedirectMapping(): RedirectMapping
     {
-        return new RedirectMapping((string) $this->config('redirectMapping'));
+        return new RedirectMapping(
+            (string) $this->config(
+                'redirectMapping',
+                $this->request->attributes->get('sw-sales-channel-id')
+            )
+        );
     }
 }
