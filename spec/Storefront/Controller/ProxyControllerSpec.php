cf6c0446d5f17f8077f5a87bc999f7a1b423e6a3
gjamroz <gjamroz@objectivity.co.uk>
2023-03-01 13:10:35 +0100

    FFWEB-2661 - Proxy

--- spec/Storefront/Controller/ProxyControllerSpec.php
+++ spec/Storefront/Controller/ProxyControllerSpec.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace spec\Omikron\FactFinder\Shopware6\Storefront\Controller;
 
+use GuzzleHttp\Psr7\Stream;
 use Omikron\FactFinder\Communication\Client\ClientBuilder;
 use Omikron\FactFinder\Communication\Client\ClientInterface;
 use Omikron\FactFinder\Shopware6\Config\Communication;
 use Omikron\FactFinder\Shopware6\Events\EnrichProxyDataEvent;
 use PhpSpec\ObjectBehavior;
 use PhpSpec\Wrapper\Collaborator;
+use PHPUnit\Framework\Assert;
+use Prophecy\Argument;
 use Psr\Http\Message\ResponseInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -17,34 +20,54 @@ use Symfony\Component\HttpFoundation\Request;
 
 class ProxyControllerSpec extends ObjectBehavior
 {
-    private Collaborator $config;
+    private Collaborator $client;
+    private Collaborator $clientBuilder;
 
-    public function let(Communication $config) {
-        $config->getServerUrl()->willReturn('https://example.fact-finder.de/fact-finder');
+    public function let(
+        Communication $config,
+        ClientInterface $client,
+        ClientBuilder $clientBuilder
+    ) {
+        $serverUrl = 'https://example.fact-finder.de/fact-finder';
+        $config->getServerUrl()->willReturn($serverUrl);
         $config->getVersion()->willReturn('ng');
         $config->getCredentials()->willReturn([
             'username',
             'pass',
         ]);
         $this->beConstructedWith($config);
+        $clientBuilder->build()->willReturn($client);
+        $clientBuilder->withServerUrl(Argument::any())->willReturn($clientBuilder);
+        $clientBuilder->withCredentials(Argument::any())->willReturn($clientBuilder);
+        $clientBuilder->withVersion(Argument::any())->willReturn($clientBuilder);
+        $this->client = $client;
+        $this->clientBuilder = $clientBuilder;
     }
 
     public function it_should_return_original_get_response(
-        Request       $request,
-        ClientBuilder $clientBuilder,
-        ClientInterface $client,
+        Request $request,
         ResponseInterface $response,
         EventDispatcherInterface $eventDispatcher,
-        EnrichProxyDataEvent $event
+        EnrichProxyDataEvent $event,
+        Stream $stream
     ) {
+        // Expect & Given
         $request->getMethod()->willReturn(Request::METHOD_GET);
-        $clientBuilder->build()->willReturn($client);
         $uri = 'rest/v5/search/example_channel?query=bag&sid=123&format=json';
-        $client->request(Request::METHOD_GET, $uri)->willReturn($response);
-        $event->getData()->willReturn([
-            'hits' => [],
-        ]);
+        $_SERVER['REQUEST_URI'] = sprintf('/fact-finder/proxy/%s', $uri);
+        $this->client->request(Request::METHOD_GET, $uri)->willReturn($response);
+        $jsonResponse = file_get_contents(dirname(__DIR__, 2) . '/data/proxy/search-bag.json');
+        $responseData = json_decode($jsonResponse, true);
+        $stream->__toString()->willReturn($jsonResponse);
+        $response->getBody()->willReturn($stream);
+        $event->getData()->willReturn($responseData);
+        $eventDispatcher->dispatch(Argument::type(EnrichProxyDataEvent::class))->willReturn($event);
 
-        $this->shouldBeAnInstanceOf(JsonResponse::class);
+        // When
+        $response = $this->execute('rest/v5/search/example_channel', $request, $this->clientBuilder, $eventDispatcher);
+
+        // Then
+        $response->shouldBeAnInstanceOf(JsonResponse::class);
+        Assert::assertEquals($responseData, json_decode($response->getWrappedObject()->getContent(), true));
     }
 }
