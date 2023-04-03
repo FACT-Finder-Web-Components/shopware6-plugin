7b613ac3b33289d70997ad6e66866b88b06b83bf
Sven Herrmann <sven.herrmann@ianeo.de>
2023-04-03 16:25:48 +0200

    cache field roles as they are searched for on every request

---
+++ src/Config/FieldRolesInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Omikron\FactFinder\Shopware6\Config;
+
+interface FieldRolesInterface
+{
+    public function getRoles(?string $salesChannelId): array;
+
+    public function update(array $fieldRoles, ?string $salesChannelId): void;
+}
