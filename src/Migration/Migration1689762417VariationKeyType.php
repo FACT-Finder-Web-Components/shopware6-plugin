d51f15b57ee15032747a0b2daef428ece607138e
Robert Saternus <robert.saternus@gmail.com>
2023-07-20 10:27:00 +0200

    FFWEB-2765: Change type for column variation_key
    Change type to LONGTEXT for column `variation_key` in table `factfinder_feed_preprocessor`

---
+++ src/Migration/Migration1689762417VariationKeyType.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Omikron\FactFinder\Shopware6\Migration;
+
+use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Migration\MigrationStep;
+
+class Migration1689762417VariationKeyType extends MigrationStep
+{
+    public function getCreationTimestamp(): int
+    {
+        return 1689762417;
+    }
+
+    public function update(Connection $connection): void
+    {
+        $query = <<<SQL
+ALTER TABLE `factfinder_feed_preprocessor` MODIFY `variation_key` LONGTEXT;
+SQL;
+
+        $connection->executeStatement($query);
+    }
+
+    /**
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function updateDestructive(Connection $connection): void
+    {
+    }
+}
