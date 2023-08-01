0c5b18dcf9f953b43749e4bb09632bb8f022aa9b
Rayn93 <robert.saternus@gmail.com>
2023-08-01 17:44:33 +0200

    Add test for feed preprocesor

--- spec/DataAbstractionLayer/FeedPreprocessorSpec.php
+++ spec/DataAbstractionLayer/FeedPreprocessorSpec.php
@@ -170,63 +170,62 @@ class FeedPreprocessorSpec extends ObjectBehavior
 ////        );
 ////    }
 //
-//    function it_should_export_only_one_entry_if_main_variant_is_selected()
-//    {
-//        $productEntity = $this->productMockFactory->create();
-//        $productEntity->setVariantListingConfig(new VariantListingConfig(null, 'SW100', null));
-//        $customFieldsData = [
-//            'SW100.1' => ['shape' => 'triangle', 'pattern' => 'stripes'],
-//            'SW100.2' => ['shape' => 'rectangle', 'pattern' => 'patterned'],
-//            'SW100.3' => ['shape' => 'square', 'pattern' => 'dots'],
-//        ];
+    function it_should_export_only_one_entry_if_main_variant_is_selected()
+    {
+        $productEntity = $this->productMockFactory->create();
+        $productEntity->setVariantListingConfig(new VariantListingConfig(null, 'SW100', null));
+        $customFieldsData = [
+            'SW100.1' => ['shape' => 'triangle', 'pattern' => 'stripes'],
+            'SW100.2' => ['shape' => 'rectangle', 'pattern' => 'patterned'],
+            'SW100.3' => ['shape' => 'square', 'pattern' => 'dots'],
+        ];
+
+        $customFieldsValues = [
+            'SW100.1' => '|shape=triangle|pattern=stripes|',
+            'SW100.2' => '|shape=rectangle|pattern=patterned|',
+            'SW100.3' => '|shape=square|pattern=dots|',
+        ];
+
+        $variants = [
+            $this->variantMockFactory->create($productEntity, ['productNumber' => 'SW100.1', 'size' => 'S', 'color' => 'red', 'material' => 'cotton', 'customFields' => $customFieldsData['SW100.1']]),
+            $this->variantMockFactory->create($productEntity, ['productNumber' => 'SW100.2', 'size' => 'M', 'color' => 'red', 'material' => 'linen', 'customFields' => $customFieldsData['SW100.1']]),
+            $this->variantMockFactory->create($productEntity, ['productNumber' => 'SW100.3', 'size' => 'L', 'color' => 'red', 'material' => 'wool', 'customFields' => $customFieldsData['SW100.1']])
+        ];
+
+        foreach ($variants as $variant) {
+            $this->customFields->getValue($variant)->willReturn($customFieldsValues[$variant->getProductNumber()]);
+        }
+
+        $productEntity->setChildren(
+            new ProductCollection(
+                array_reduce($variants, fn(array $carriedVariants, ProductEntity $product): array
+                => $carriedVariants + [$product->getId() => $product], [])));
+
+        /** @var Subject $entries */
+        $entries = $this->createEntries($productEntity, $this->context);
+        $entries->shouldBeArray();
+        //only one entry should be producted
+        $entries->shouldHaveCount(1);
+
+        $this->validateFilterAttributes($entries->getWrappedObject(), [
+            'size=S|size=M|size=L|color=red|material=cotton|material=linen|material=wool'
+        ]);
+
+
+    }
 //
-//        $customFieldsValues = [
-//            'SW100.1' => '|shape=triangle|pattern=stripes|',
-//            'SW100.2' => '|shape=rectangle|pattern=patterned|',
-//            'SW100.3' => '|shape=square|pattern=dots|',
-//        ];
-//
-//        $variants = [
-//            $this->variantMockFactory->create($productEntity, ['productNumber' => 'SW100.1', 'size' => 'S', 'color' => 'red', 'material' => 'cotton', 'customFields' => $customFieldsData['SW100.1']]),
-//            $this->variantMockFactory->create($productEntity, ['productNumber' => 'SW100.2', 'size' => 'M', 'color' => 'red', 'material' => 'linen', 'customFields' => $customFieldsData['SW100.1']]),
-//            $this->variantMockFactory->create($productEntity, ['productNumber' => 'SW100.3', 'size' => 'L', 'color' => 'red', 'material' => 'wool', 'customFields' => $customFieldsData['SW100.1']])
-//        ];
-//
-//        foreach ($variants as $variant) {
-//            $this->customFields->getValue($variant)->willReturn($customFieldsValues[$variant->getProductNumber()]);
-//        }
-//
-//        $productEntity->setChildren(
-//            new ProductCollection(
-//                array_reduce($variants, fn(array $carriedVariants, ProductEntity $product): array
-//                => $carriedVariants + [$product->getId() => $product], [])));
-//
-//        /** @var Subject $entries */
-//        $entries = $this->createEntries($productEntity, $this->context);
-//        $entries->shouldBeArray();
-//        //only one entry should be producted
-//        $entries->shouldHaveCount(1);
-//
-//        $this->validateFilterAttributes($entries->getWrappedObject(), [
-//            'size=S|size=M|size=L|color=red|material=cotton|material=linen|material=wool'
-//        ]);
-//
-//
-//    }
-//
-//    private function validateFilterAttributes(array $entries, array $filters)
-//    {
-//        var_dump($entries); die();
-//        $expected = count($filters);
-//        $isContainingFilter = fn(string $filter): callable
-//            => fn(array $entry): bool
-//                => strpos($entry['filterAttributes'], $filter) !== false;
-//
-//        $match = array_reduce($filters, fn (int $score, string $filter)
-//            => $score + (int) array_filter($entries, $isContainingFilter($filter)), 0);
-//
-//        Assert::assertEquals($expected, $match);
-//    }
+    private function validateFilterAttributes(array $entries, array $filters)
+    {
+        $expected = count($filters);
+        $isContainingFilter = fn(string $filter): callable
+            => fn(array $entry): bool
+                => strpos($entry['filterAttributes'], $filter) !== false;
+
+        $match = array_reduce($filters, fn (int $score, string $filter)
+            => $score + (int) array_filter($entries, $isContainingFilter($filter)), 0);
+
+        Assert::assertEquals($expected, $match);
+    }
 //
 //    private function validateCustomFields(array $groupConfigurationConfig, array $expectedValues)
 //    {
