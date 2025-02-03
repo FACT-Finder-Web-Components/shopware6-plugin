aad5dc385b975f35788e1395f6a9c9a8d2fb9cb9
Robert Saternus <robert.saternus@gmail.com>
2025-02-03 11:19:52 +0100

    FFWEB-3305: Support Proxy feature for Server Side Rendering
    Support Proxy feature for Server Side Rendering

--- src/Utilites/Ssr/SearchAdapter.php
+++ src/Utilites/Ssr/SearchAdapter.php
@@ -8,21 +8,26 @@ use Omikron\FactFinder\Communication\Client\ClientBuilder;
 use Omikron\FactFinder\Communication\Client\ClientException;
 use Omikron\FactFinder\Shopware6\Config\Communication;
 use Psr\Http\Message\ResponseInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
 
 class SearchAdapter
 {
     private ClientBuilder $clientBuilder;
     private Communication $config;
     private PriceFormatter $priceFormatter;
+    private RouterInterface $router;
 
     public function __construct(
         ClientBuilder $clientBuilder,
         Communication $config,
         PriceFormatter $priceFormatter,
+        RouterInterface $router,
     ) {
         $this->clientBuilder  = $clientBuilder;
         $this->config         = $config;
         $this->priceFormatter = $priceFormatter;
+        $this->router         = $router;
     }
 
     public function search(
@@ -31,7 +36,7 @@ class SearchAdapter
         string $salesChannelId,
     ): array {
         $client = $this->clientBuilder
-            ->withServerUrl($this->config->getServerUrl())
+            ->withServerUrl($this->getServerUrl())
             ->withApiKey($this->config->getApiKey())
             ->withVersion($this->config->getVersion())
             ->build();
@@ -58,5 +63,18 @@ class SearchAdapter
         $endpoint   = $navigationRequest ? 'navigation' : 'search';
 
         return "rest/{$apiVersion}/{$endpoint}/{$channel}?{$paramString}";
+    }
+
+    private function getServerUrl(): string
+    {
+        if ($this->config->isProxyEnabled()) {
+            return $this->router->generate(
+                'frontend.factfinder.proxy.execute',
+                ['endpoint' => ''],
+                UrlGeneratorInterface::ABSOLUTE_URL
+            );
+        }
+
+        return $this->config->getServerUrl();
     }
 }
