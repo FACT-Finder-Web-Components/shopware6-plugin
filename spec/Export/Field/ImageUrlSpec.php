3e9d64315c4476dcce16bbfb02ad70fe79629574
robertsaternus <robert.saternus@gmail.com>
2025-07-03 14:05:27 +0200

    Update

--- spec/Export/Field/ImageUrlSpec.php
+++ spec/Export/Field/ImageUrlSpec.php
@@ -14,26 +14,27 @@ use Shopware\Core\Content\Product\SalesChannel\SalesChannelProductEntity as Prod
 
 class ImageUrlSpec extends ObjectBehavior
 {
-    public function it_is_a_field()
+    public function it_is_a_field(): void
     {
         $this->shouldBeAnInstanceOf(FieldInterface::class);
     }
 
-    public function it_has_a_name()
+    public function it_has_a_name(): void
     {
         $this->getName()->shouldReturn('ImageUrl');
     }
 
     public function it_does_not_fail_if_the_image_is_not_present(
-        Product                $product,
+        Product $product,
         ProductMediaCollection $mediaCollection,
-        ProductMediaEntity     $productMediaEntity,
-        Media                  $media
-    ) {
+        ProductMediaEntity $productMediaEntity,
+        Media $media,
+    ): void {
         $this->shouldNotThrow()->during('getValue', [$product]);
         $this->getValue($product)->shouldReturn('');
 
         $product->getMedia()->willReturn($mediaCollection);
+        $product->getCover()->willReturn($productMediaEntity);
         $mediaCollection->first()->willReturn($productMediaEntity);
         $productMediaEntity->getMedia()->willReturn($media);
 
@@ -41,26 +42,10 @@ class ImageUrlSpec extends ObjectBehavior
         $this->getValue($product)->shouldReturn('/product_image.jpg');
     }
 
-    public function it_does_not_fail_if_media_is_not_a_collection(CategoryEntity $category, Media $media)
+    public function it_does_not_fail_if_media_is_not_a_collection(CategoryEntity $category, Media $media): void
     {
         $category->getMedia()->willReturn($media);
         $media->getUrl()->willReturn('/category_image.jpg');
         $this->getValue($category)->shouldReturn('/category_image.jpg');
     }
-
-    public function it_does_not_fail_if_media_first_is_null(Product $product, ProductMediaCollection $mediaCollection)
-    {
-        $product->getMedia()->willReturn($mediaCollection);
-        $mediaCollection->first()->willReturn(null);
-
-        $this->shouldNotThrow()->during('getValue', [$product]);
-    }
-
-    public function it_does_not_fail_if_media_is_null(Product $product)
-    {
-        $product->getMedia()->willReturn(null);
-
-        $this->shouldNotThrow()->during('getValue', [$product]);
-    }
-
 }
